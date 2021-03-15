1bcb1223883215e23911a383aa3b9cab8aeca7a7
Genara <genara7sousa@gmail.com>
2021-03-15 01:26:24 -0300

    Last adjustments.

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+//Manipulação de dados da persistência de dados fica aqui
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,35 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce(
+      (sum: Balance,transaction: Transaction) => {
+        switch(transaction.type){
+          case 'income':
+            sum.income += transaction.value;
+            break;
+          case 'outcome':
+            sum.outcome += transaction.value; 
+        }
+        sum.total = sum.income - sum.outcome;
+       return sum;
+      }, 
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      }
+    )
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ value, type, title });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,14 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+
+// Parte de regras de negócio da criação de transação fica aqui
+interface dataTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+} 
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +16,15 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({title,type,value}: dataTransaction): Transaction {
+    const transaction = this.transactionsRepository.create({title, value, type});
+    if(type === 'outcome'){
+      const balance = this.transactionsRepository.getBalance();
+      if(balance.total < value){
+        throw Error('The outcome value exceeds the limit of the total.');
+      }
+    }
+    return transaction;
   }
 }
 
